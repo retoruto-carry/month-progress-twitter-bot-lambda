81bedeb50bc21accbfc7684ff35cedb2e2703702
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-02-28 23:23:22 +0900

    chage response word

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'changed hello world',
                 // location: ret.data.trim()
             })
         }
