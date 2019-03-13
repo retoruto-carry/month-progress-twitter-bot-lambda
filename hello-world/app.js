4bc264b5f8d4fb5f3619b0a168a91ede3182147e
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-03-13 20:00:58 +0900

    cleanUp

--- hello-world/app.js
+++ hello-world/app.js
@@ -16,6 +16,73 @@ let response;
  */
 exports.lambdaHandler = (event, context) => {
 
+    // tweet(
+    //     generateTweetText(
+    //         calcPassedTimeRatio()
+    //     )
+    // );
+
+};
+
+/**
+ * 現在時刻、当月がどれだけの割合経過しているかを計算する
+ * @return float - 経過した割合(0~100%)
+ */
+// function calcPassedTimeRatio() {
+
+//     return passedTimeRatio;
+// }
+
+/**
+ * 実際にツイートするテキストを生成する
+ * 例えばこんなツイート--------
+ * 1月
+ * ▓▓░░░░░░░░░░░░░ 13%
+ * -------------------------
+ * @param passedTimeRatio - 経過した割合(0~100％)
+ */
+function generateTweetText(passedTimeRatio) {
+
+    tweetText = `${generateProgressBar(passedTimeRatio)} ${passedTimeRatio}%`;
+
+    return tweetText;
+}
+
+/**
+ * プログレスバーのテキストを生成する
+ * 例えばこんなテキスト--------
+ * ▓▓░░░░░░░░░░░░░
+ * -------------------------
+ * ブロックを塗る数は四捨五入で決定する
+ * @param passedTimeRatio - 何%時間が過ぎたか 0~100(％)
+ */
+function generateProgressBar(passedTimeRatio) {
+
+    const blockWidth = 15;
+
+    filledBlockWidth = Math.round(passedTimeRatio / (100　/　blockWith));
+    console.log("filledBlockWidth: " + filledBlockWidth);
+
+    progressBar = '';
+
+    // プログレスバーのうち塗られている部分
+    for (i = 0; i < filledBlockWidth; i++) {
+        progressBar += '▓';
+    }
+
+    // プログレスバーのうち塗られていない部分
+    for (i = 0; i < blockWith - filledBlockWidth; i++) {
+        progressBar += '░';
+    }
+
+    return progressBar;
+}
+
+/**
+ * 与えられた文字列をツイートする
+ * @param String text - ツイートする文字列
+ */
+function tweet(text) {
     var Twitter = require('twitter');
  
     var client = new Twitter({
@@ -25,45 +92,15 @@ exports.lambdaHandler = (event, context) => {
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
 
-    client.post('statuses/update', {status: 'this is a test'}, function(error, tweet, response) {
+    client.post('statuses/update', {status: text}, function(error, tweet, response) {
 
         if (error) {
-            // console.log(error);
-            // throw error;
             console.log(error)
-            // return context.fail("fail")
+            throw error;
         }
 
         console.log(tweet);
         console.log(response);
 
-        // context.succeed("success");
     })
-
-    console.log(process.env.SAMPLE_KEY);
-    console.log(genarateTweetText(90));
-
-};
-
-// ツイートテキスト生成
-// 例　▓▓░░░░░░░░░░░░░ 13% 
-// 15個のブロック ブロック数は四捨五入
-function genarateTweetText(passedTimeRatio) {
-
-    passedTimeRatioBlockAmount = Math.round(passedTimeRatio / (100/15))
-    console.log("passedTimeRatioBlockAmount: " + passedTimeRatioBlockAmount)
-
-    text = ''
-
-    for (i = 0; i < passedTimeRatioBlockAmount; i++) {
-        text += '▓'
-    }
-
-    for (i = 0; i < 15 - passedTimeRatioBlockAmount; i++) {
-        text += '░'
-    }
-
-    text += ` ${passedTimeRatio}%`
-
-    return text
 }
