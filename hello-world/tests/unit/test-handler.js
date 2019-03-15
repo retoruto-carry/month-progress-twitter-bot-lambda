f08e8bf98dfcddcd69e7aaffa640ae715986ece4
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-03-15 16:00:27 +0900

    add: testを追加(NOT WORKING)

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -2,21 +2,12 @@
 
 const app = require('../../app.js');
 const chai = require('chai');
-const expect = chai.expect;
+var assert = chai.assert;
 var event, context;
 
 describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
-
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("changed hello world");
-        // expect(response.location).to.be.an("string");
+    // not working 書き方が分からない
+    it('generateProgressBar_0%', async () => {
+        assert.deepEqual(await app.generateProgressBar(0),'░░░░░░░░░░░░░░░');
     });
 });
