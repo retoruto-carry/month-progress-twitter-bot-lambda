68027e4cda55afad39427f66afc3ab266a45fa10
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-04-29 14:51:21 +0900

    refactor: コメントのJSDocを修正

--- src/app.js
+++ src/app.js
@@ -8,12 +8,9 @@ moment.tz.setDefault('Asia/Tokyo')
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
  */
 exports.lambdaHandler = (event, context) => {
 
@@ -26,8 +23,8 @@ exports.lambdaHandler = (event, context) => {
 };
 
 /**
- * 現在時刻、当月がどれだけの割合経過しているかを計算する
- * @return float - 経過した割合(0~100%)
+ * 現在時刻から、「当月がどれだけの割合経過しているか」を計算する
+ * @return {Number} 経過した割合(0~1)
  */
 exports.calcPassedTimeRatio = () => {
 
@@ -48,13 +45,14 @@ exports.calcPassedTimeRatio = () => {
 
 /**
  * 実際にツイートするテキストを生成する
- * 例えばこんなツイート--------
+ * 例えばこんなツイート-----
  * 1月
  * ▓▓░░░░░░░░░░░░░ 13%
  * -------------------------
- * @param float passedTimeRatio - 経過した割合 (0~1)
+ * @param  {Number} passedTimeRatio 経過した割合(0~1)
+ * @return {string}
  */
-exports.generateTweetText = (passedTimeRatio) => {    
+exports.generateTweetText = (passedTimeRatio) => {
 
     const month = moment().format('M')
     const progressBar = this.generateProgressBar(passedTimeRatio);
@@ -66,11 +64,12 @@ exports.generateTweetText = (passedTimeRatio) => {
 
 /**
  * プログレスバーのテキストを生成する
- * 例えばこんなテキスト--------
+ * 例えばこんなテキスト-----
  * ▓▓░░░░░░░░░░░░░
  * -------------------------
  * ブロックを塗る数は四捨五入で決定する
- * @param passedTimeRatio - 何%時間が過ぎたか (0~1)
+ * @param {Number} passedTimeRatio ブロックを塗る割合(0~1)
+ * @retun {String}
  */
 exports.generateProgressBar = (passedTimeRatio) => {
 
@@ -84,12 +83,12 @@ exports.generateProgressBar = (passedTimeRatio) => {
 
 /**
  * 与えられた文字列をツイートする
- * @param String text - ツイートする文字列
+ * @param {String} text ツイートする文字列
  */
 exports.doTweet = (text) => {
 
     const Twitter = require('twitter');
- 
+
     const client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
